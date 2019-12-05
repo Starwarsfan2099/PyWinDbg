72aaf259c042faf2b75f780045f3ff3c328a12b0
Starwarsfan2099 <starwarsfan2099@gmail.com>
2019-12-04 21:24:45 -0500

    Temp fix dependencies and fix a printing issue.

--- build_buffer_overflow.bat
+++ build_buffer_overflow.bat
@@ -2,7 +2,7 @@
 echo.
 echo Building bufferOverflow...
 echo.
-pyinstaller bufferOverflow.py -F --upx-dir C:\Users\Owner\Downloads\upx-3.95-win64\upx-3.95-win64 --distpath Executables
+pyinstaller bufferOverflow.py -F --upx-dir C:\Users\Starw\Downloads\upx-3.95-win64\upx-3.95-win64 --distpath Executables
 echo.
 echo Done!
 echo.
--- build_pywindbg.bat
+++ build_pywindbg.bat
@@ -2,7 +2,7 @@
 echo.
 echo Building PyWindDbg...
 echo.
-pyinstaller pywindbg.py -F --upx-dir C:\Users\Owner\Downloads\upx-3.95-win64\upx-3.95-win64  --distpath Executables
+pyinstaller pywindbg.py -F --upx-dir C:\Users\Starw\Downloads\upx-3.95-win64\upx-3.95-win64  --distpath Executables
 echo.
 echo Done!
 echo.

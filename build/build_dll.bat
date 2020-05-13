c443a5c14d8159be0738f6ec64fa342c8ac0176a
Starwarsfan2099 <starwarsfan2099@gmail.com>
2020-05-12 21:42:42 -0400

    Cleanup dll building some

--- build/build_dll.bat
+++ build/build_dll.bat
@@ -2,7 +2,8 @@
 echo.
 echo [92mBuilding DLL...[0m
 echo.
-cl.exe /D_USRDLL /D_WINDLL ..\helloDLL.cpp /link /DLL /OUT:..\Executables\helloDLL.dll
+if not exist "tmp" mkdir tmp
+cl.exe /Fo"tmp\\" /D_USRDLL /D_WINDLL ..\helloDLL.cpp /link /DLL /OUT:..\Executables\helloDLL.dll
 echo.
 echo [92mDone![0m
 echo.
